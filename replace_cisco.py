eae573b6ef3818023ee02e55370a44e659a44e27
borekbp <borekbp@gmail.com>
2019-07-24 23:49:37 +0200

    za pomoca pycharm

--- replace_cisco.py
+++ replace_cisco.py
@@ -10,9 +10,9 @@ infile_replace = open(pliknowy, 'w')
 for line in infile: infile_replace.write(re.sub(" switchport mode trunk", "mode_trunk",
                             re.sub(" switchport mode access", "mode_access",
                             re.sub(" authentication port-control auto", "dot1x_ok",
+                            re.sub(" switchport access vlan ", "vlan_",
                             re.sub(" switchport access vlan 228", "vlan_ok",
                             re.sub(" switchport access vlan 231", "vlan_ok",
-                            re.sub(" switchport access vlan ", "vlan_",
                             re.sub("interface Gigabitethernet", nameorg + "_Gi",
                             line))))))))
 
